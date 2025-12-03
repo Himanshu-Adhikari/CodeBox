a608e950826de04f7a93d79a3f1d4c41f07768c5
Himanshu-Adhikari <adhikarihimanshu393@gmail.com>
2025-12-03 09:01:05 +0530

    updated a few changes

--- app/provider.tsx
+++ app/provider.tsx
@@ -3,31 +3,38 @@
 import React, { useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useUser } from "@clerk/nextjs";
-import axios from 'axios'
-import { UserDetailContext } from "@/context/UserDetailContext";
+import axios from "axios";
+import { UserDetail, UserDetailContext } from "@/context/UserDetailContext";
 
 const Provider = ({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) => {
-  const {user}=useUser();
-  const [userDetail,setUserDetail]=useState();
+  const { user, isLoaded } = useUser();
+  const [userDetail, setUserDetail] = useState<UserDetail | undefined>();
 
-  useEffect(()=>{
-    user&&createNewUser()
-  },[user])
+  useEffect(() => {
+    if (isLoaded && user) {
+      createOrFetchUser();
+    }
+  }, [isLoaded, user]);
 
-  const createNewUser=async ()=>{
-    const result=await axios.post('/api/user',{});
-    console.log(result)
-    setUserDetail(result.data)
-  }
-  return <NextThemesProvider {...props}>
-    <UserDetailContext.Provider value={{userDetail,setUserDetail}}>
+  const createOrFetchUser = async () => {
+    try {
+      const res = await axios.post("/api/user");
+      setUserDetail(res.data);
+    } catch (err) {
+      console.error("Failed to create/fetch user", err);
+    }
+  };
 
-    {children}
-    </UserDetailContext.Provider>
-    </NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+        {children}
+      </UserDetailContext.Provider>
+    </NextThemesProvider>
+  );
 };
 
 export default Provider;
--- context/UserDetailContext.tsx
+++ context/UserDetailContext.tsx
@@ -1,6 +1,24 @@
-import { createContext } from "react";
+"use client";
 
-export const UserDetailContext=createContext<any>({
-    userDetail:undefined,
-    setUserDetail:()=>{}
-})
+import { createContext, useContext, useState } from "react";
+
+export interface UserDetail {
+  id: number;
+  name: string;
+  email: string;
+  points: number;
+  subscription: string | null;
+}
+
+interface UserDetailContextType {
+  userDetail: UserDetail | undefined;
+  setUserDetail: (user: UserDetail | undefined) => void;
+}
+
+export const UserDetailContext = createContext<UserDetailContextType>({
+  userDetail: undefined,
+  setUserDetail: () => {},
+});
+
+// Optional but recommended: custom hook
+export const useUserDetail = () => useContext(UserDetailContext);
