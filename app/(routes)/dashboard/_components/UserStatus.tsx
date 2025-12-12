2caaf1252ad407cd0e1051c0a5d721c187dccd65
Himanshu-Adhikari <adhikarihimanshu393@gmail.com>
2025-12-12 09:10:07 +0530

    normal changes in UI like stars aligning

--- app/(routes)/dashboard/_components/UserStatus.tsx
+++ app/(routes)/dashboard/_components/UserStatus.tsx
@@ -13,24 +13,24 @@ const UserStatus = () => {
             </h2>
     </div>
     <div className='grid grid-cols-2 gap-5'>
-    <div className='flex items-center gap-3'>
+    <div className='flex items-center '>
         <Image src={'/star.gif'} alt='star' width={35} height={35}></Image>
         <div>
-            <h2 className='font-xl font-game'>20</h2>
+            <h2 className='text-xl font-game'>20</h2>
             <h2 className='font-game text-xl text-gray-600'>Total Rewards</h2>
         </div>
     </div>
     <div className='flex items-center gap-3'>
         <Image src={'/badge_2.png'} alt='badge' width={35} height={35}></Image>
         <div>
-            <h2 className='font-xl font-game'>3</h2>
+            <h2 className='text-xl font-game'>3</h2>
             <h2 className='font-game text-xl text-gray-600'>Badge</h2>
         </div>
     </div>
     <div className='flex items-center gap-3'>
         <Image src={'/fire.gif'} alt='fire' width={35} height={35}></Image>
         <div>
-            <h2 className='font-xl font-game'>7</h2>
+            <h2 className='text-xl font-game'>7</h2>
             <h2 className='font-game text-xl text-gray-600'>Streak</h2>
         </div>
     </div>
